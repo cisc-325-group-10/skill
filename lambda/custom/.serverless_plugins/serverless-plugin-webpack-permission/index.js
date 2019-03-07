5337ce6e997ea4341fb5c1608ced9a27e7689f94
Justin Gerhardt <justin@gerhardt.link>
2019-03-07 10:46:09 -0500

    Check OS before running webpack permission script

--- lambda/custom/.serverless_plugins/serverless-plugin-webpack-permission/index.js
+++ lambda/custom/.serverless_plugins/serverless-plugin-webpack-permission/index.js
@@ -3,20 +3,25 @@
 const { execSync } = require('child_process');
 
 class Plugin {
-  constructor(serverless, options) {
-    this.serverless = serverless;
-    this.options = options;
+    constructor(serverless, options) {
+        this.serverless = serverless;
+        this.options = options;
 
-    this.hooks = {
-      'before:webpack:package:packageModules': this.deployCompileEvents.bind(this)
-    };
-  }
+        this.hooks = {
+            'before:webpack:package:packageModules': this.deployCompileEvents.bind(this)
+        };
+    }
 
-  deployCompileEvents() {
-    this.serverless.cli.log("RUNNING PERMISSION CHANGE");
-    execSync("chmod -R ugo+rwx ./.webpack");
-  }
-
+    deployCompileEvents() {
+        this.serverless.cli.log("RUNNING PERMISSION CHANGE");
+        switch (os.type()) {
+            case "Linux":
+                execSync("chmod -R ugo+rwx ./.webpack");
+                break;
+            default:
+                this.serverless.cli.log("PERMISSION CHANGE DOESN'T SUPPORT THIS OS");
+        }
+    }
 }
 
 module.exports = Plugin;
